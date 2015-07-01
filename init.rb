b1c95b17afab87eb76b9ed1d96c76f2dd0a7266f
ste26054 <ste26054@gmail.com>
2015-07-01 15:20:24 +0100

    init

---
+++ init.rb
@@ -0,0 +1,6 @@
+Redmine::Plugin.register :redmine_workload_capacity do
+  name 'Redmine Workload Capacity plugin'
+  author 'Stephane Evrard'
+  description 'This is a plugin for Redmine'
+  version '0.0.1'
+end
