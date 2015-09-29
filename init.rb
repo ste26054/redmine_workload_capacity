fcb8c2c76ee3fbc9f9a1eb4eef8a28398afbce20
ste26054 <ste26054@gmail.com>
2015-09-29 10:01:23 +0100

    update init for version

--- init.rb
+++ init.rb
@@ -3,11 +3,12 @@ require 'redmine_workload_capacity'
 
 Redmine::Plugin.register :redmine_workload_capacity do
 	requires_redmine_plugin :redmine_leaves_holidays, :version_or_higher => '0.1'
+	requires_redmine :version_or_higher => "3.0.0"
 
   name 'Redmine Allocation Capacity plugin'
   author 'Stephane Evrard'
   description 'A Redmine Plugin for managing work capacity across projects'
-  version '0.0.1'
+  version '0.1'
 
   menu :project_menu, :workload, { :controller => 'wl_boards', :action => 'index'},
                               :caption => :label_workload,
