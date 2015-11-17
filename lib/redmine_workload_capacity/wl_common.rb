fab59c3584e973ea09b16ba218460af52ec1d54a
Soniaz08 <zhao.soniaa@gmail.com>
2015-11-17 17:00:26 +0000

    add new permision : view_global_allocation_check There are 4 states: right to manage allocation: able to see all tabs, configure allocations * right to view global check allocation: able to see check tab only with all the project members of the project window* implicit right for project member(selected Roles to be displayed on the project window at its definition): able to see check tab only with its own data only* others: the module Allocation is not visible*

--- lib/redmine_workload_capacity/wl_common.rb
+++ lib/redmine_workload_capacity/wl_common.rb
@@ -1,5 +1,5 @@
 module WlCommon
 	def authenticate
-    	render_403 unless User.current.wl_manage_right?(@project) || User.current.members.where(project_id:  @project.id).first.wl_member?
+    	render_403 unless User.current.wl_manage_right?(@project) || User.current.allowed_to?(:view_global_allocation_check, @project) || User.current.members.where(project_id:  @project.id).first.wl_member?
   	end
 end
